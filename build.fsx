806d4b1b4745c8e0beabc71b2cfb38f114e0b2d9
Feihong Hsu <feihong.hsu@gmail.com>
2017-02-21 21:26:25 -0600

    Add build file

---
+++ build.fsx
@@ -0,0 +1,22 @@
+// include Fake lib
+#r @"packages/FAKE/tools/FakeLib.dll"
+open Fake
+
+// Properties
+let buildDir = "./build/"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "Default" (fun _ ->
+    trace "Hello World from FAKE"
+)
+
+// Dependencies
+"Clean"
+    ==> "Default"
+
+// start build
+RunTargetOrDefault "Default"
